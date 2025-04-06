5513cae91f5444b7c5e7649611b0b61cc453dd46
sy.royce <sham@dotcod.in>
2025-04-06 16:22:48 +0530

    next config upate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint warnings during the build
+  },
 };
 
 export default nextConfig;
